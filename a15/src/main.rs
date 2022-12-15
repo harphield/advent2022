2497fcb6a593e7b3da5a50ac4d6a6d2189037e17
harphield <harphield@gmail.com>
2022-12-15 22:55:02 +0100

    Day 15 part 2

--- a15/src/main.rs
+++ a15/src/main.rs
@@ -5,9 +5,8 @@ use std::io;
 use std::io::BufRead;
 use std::ops::Sub;
 
-const SEARCH_ROW: i64 = 2000000;
 const MIN_COORD: i64 = 0;
-const MAX_COORD: i64 = 20;
+const MAX_COORD: i64 = 4000000;
 
 #[derive(Debug)]
 struct Point {
@@ -21,9 +20,7 @@ fn main() -> Result<(), io::Error> {
     let rx =
         Regex::new(r"^Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)$").unwrap();
 
-    let mut beacons_on_row = HashSet::new();
-
-    let mut cannot = HashSet::new();
+    let mut potential: Vec<HashSet<(i64, i64)>> = vec![HashSet::new(); (MAX_COORD + 1) as usize];
 
     for line_r in io::BufReader::new(file).lines() {
         match line_r {
@@ -35,37 +32,94 @@ fn main() -> Result<(), io::Error> {
                 let b_x = caps.get(3).unwrap().as_str().parse::<i64>().unwrap();
                 let b_y = caps.get(4).unwrap().as_str().parse::<i64>().unwrap();
 
-                if b_y == SEARCH_ROW {
-                    beacons_on_row.insert(b_x);
-                }
-
                 let start = Point { x: s_x, y: s_y };
-
                 let distance = manhattan(&start, &Point { x: b_x, y: b_y });
 
-                match get_collision_points_with_row(&start, distance, SEARCH_ROW) {
-                    None => {}
-                    Some(points) => {
-                        if points.len() == 2 {
-                            for i in points[0].x..=points[1].x {
-                                cannot.insert(i);
+                let min_x = *[s_x - distance, MIN_COORD].iter().max().unwrap(); // 0 or more
+                let max_x = *[s_x + distance, MAX_COORD].iter().min().unwrap(); // 20 or less
+                let min_y = *[s_y - distance, MIN_COORD].iter().max().unwrap();
+                let max_y = *[s_y + distance, MAX_COORD].iter().min().unwrap();
+
+                for y in min_y..=max_y {
+                    match get_collision_points_with_row(&start, distance, y) {
+                        None => {}
+                        Some(points) => {
+                            if points.len() == 2 {
+                                if points[0].x <= max_x && points[1].x >= min_x {
+                                    let limits = (
+                                        if points[0].x >= MIN_COORD {
+                                            points[0].x
+                                        } else {
+                                            MIN_COORD
+                                        },
+                                        if points[1].x <= MAX_COORD {
+                                            points[1].x
+                                        } else {
+                                            MAX_COORD
+                                        },
+                                    );
+
+                                    potential[y as usize].insert(limits);
+                                }
+                            } else if points.len() == 1
+                                && points[0].x >= MIN_COORD
+                                && points[0].x <= MAX_COORD
+                            {
+                                potential[y as usize].insert((points[0].x, points[0].x));
                             }
-                        } else if points.len() == 1 {
-                            cannot.insert(points[0].x);
                         }
-                    }
-                };
+                    };
+                }
             }
             Err(_) => break,
         }
     }
 
-    // let mut v:Vec<&i64> = cannot.iter().collect();
-    // v.sort();
-    //
-    // println!("{:?}", v);
+    for (y, row) in potential.iter().enumerate() {
+        if row.is_empty() {
+            continue;
+        }
 
-    println!("Part 1: {}", cannot.len() - beacons_on_row.len());
+        if row.len() == 1 {
+            // hmmm
+            continue;
+        }
+
+        let mut row_vec = row.iter().collect::<Vec<&(i64, i64)>>();
+        row_vec.sort();
+
+        let mut bubbles = vec![];
+
+        let mut current_bubble = (row_vec[0].0, row_vec[0].1);
+        for (x1, x2) in row_vec {
+            // gap?
+            if *x1 > current_bubble.1 + 1 || *x2 < current_bubble.0 - 1 {
+                bubbles.push(current_bubble);
+                current_bubble = (*x1, *x2);
+                continue;
+            }
+
+            if *x1 < current_bubble.0 {
+                current_bubble.0 = *x1;
+            }
+
+            if *x2 > current_bubble.1 {
+                current_bubble.1 = *x2;
+            }
+        }
+
+        if bubbles.is_empty() {
+            continue;
+        }
+
+        bubbles.push(current_bubble);
+
+        for window in bubbles.windows(2) {
+            for x in (window[0].1 + 1)..window[1].0 {
+                println!("{}", x * 4000000 + y as i64);
+            }
+        }
+    }
 
     Ok(())
 }
@@ -88,7 +142,7 @@ fn manhattan(a: &Point, b: &Point) -> i64 {
 fn get_collision_points_with_row(start: &Point, distance: i64, y: i64) -> Option<Vec<Point>> {
     let direct_up = Point { x: start.x, y };
 
-    let distance_to_row = manhattan(&start, &direct_up);
+    let distance_to_row = manhattan(start, &direct_up);
 
     if distance < distance_to_row {
         return None;
