1cd8bf34dc66fc36adcc6ca0f4ce27c4f0e0740a
harphield <harphield@gmail.com>
2022-12-17 23:25:59 +0100

    Day 16 part 1!

--- a16/src/main.rs
+++ a16/src/main.rs
@@ -1,6 +1,5 @@
-use itertools::Itertools;
-use std::collections::HashMap;
 use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -16,8 +15,8 @@ type ValveHolder = HashMap<String, Valve>;
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
 
-    let rx =
-        Regex::new(r"^Valve (.+) has flow rate=(.+); tunnel[s]? lead[s]? to valve[s]? (.+)$").unwrap();
+    let rx = Regex::new(r"^Valve (.+) has flow rate=(.+); tunnel[s]? lead[s]? to valve[s]? (.+)$")
+        .unwrap();
 
     let mut valves: ValveHolder = HashMap::new();
 
@@ -28,16 +27,17 @@ fn main() -> Result<(), io::Error> {
 
                 let name = caps.get(1).unwrap().as_str();
                 let rate = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
-                let neighbors: Vec<(String, u32)> = caps.get(3).unwrap().as_str().split(", ").map(|v| {
-                    (v.to_string(), 0)
-                }).collect();
+                let neighbors: Vec<(String, u32)> = caps
+                    .get(3)
+                    .unwrap()
+                    .as_str()
+                    .split(", ")
+                    .map(|v| (v.to_string(), 0))
+                    .collect();
 
-                valves.insert(name.to_string(), Valve {
-                    rate,
-                    neighbors,
-                });
+                valves.insert(name.to_string(), Valve { rate, neighbors });
             }
-            Err(_) => break
+            Err(_) => break,
         }
     }
 
@@ -45,13 +45,11 @@ fn main() -> Result<(), io::Error> {
     let mut weighted_valves = ValveHolder::new();
     // find nodes that have rate > 0 + the starting node
     // find shortest paths between all of them
-    let important: Vec<String> = valves.iter().filter(|(s, v)| {
-        &s[..] == "AA" || v.rate > 0
-    }).map(|(s, _v)| {
-        s.to_string()
-    }).collect();
-
-    println!("imp {:?}", important);
+    let important: Vec<String> = valves
+        .iter()
+        .filter(|(s, v)| &s[..] == "AA" || v.rate > 0)
+        .map(|(s, _v)| s.to_string())
+        .collect();
 
     for s in &important {
         let mut s_valve = valves.get(s).unwrap().clone();
@@ -70,35 +68,73 @@ fn main() -> Result<(), io::Error> {
     }
 
     for (_s, v) in weighted_valves.iter_mut() {
-        let updated: Vec<(String, u32)> = v.neighbors.iter().filter(|(_s, w)| {
-            *w > 0u32
-        }).map(|(s, w)| {
-            (s.to_string(), *w)
-        }).collect();
+        let updated: Vec<(String, u32)> = v
+            .neighbors
+            .iter()
+            .filter(|(_s, w)| *w > 0u32)
+            .map(|(s, w)| (s.to_string(), *w))
+            .collect();
 
         v.neighbors = updated;
     }
 
-    let important_without_a: Vec<String> = important.clone().iter().filter(|v| {
-        **v != "AA".to_string()
-    }).map(|v| {
-        v.to_string()
-    })
-    .collect();
-
-    let mut max_p = 0;
-    for order in important_without_a.iter().permutations(important_without_a.len()).unique() {
-        // println!("{:?}", order);
-        let p = order_to_pressure(&order, &weighted_valves);
-
-        if p > max_p {
-            max_p = p;
-        }
-    }
+    let max_p = permutations(
+        weighted_valves.get(&"AA".to_string()).unwrap(),
+        0,
+        &vec![],
+        &weighted_valves,
+        important
+            .iter()
+            .filter(|v| v != &"AA")
+            .count(),
+    );
 
     println!("Part 1: {}", max_p);
 
     Ok(())
+}
+
+fn permutations(
+    current: &Valve,
+    minute: u32,
+    opened: &Vec<&String>,
+    valves: &ValveHolder,
+    finish_on: usize,
+) -> u32 {
+    if opened.len() == finish_on {
+        return order_to_pressure(opened, valves);
+    }
+
+    let mut values = vec![];
+
+    for neighbor in &current.neighbors {
+        if neighbor.0.eq(&"AA") || opened.contains(&&neighbor.0) {
+            continue;
+        }
+
+        if minute + neighbor.1 > 30 {
+            values.push(order_to_pressure(opened, valves));
+            continue;
+        }
+
+        let n = valves.get(&neighbor.0).unwrap();
+
+        let mut new_opened = opened.clone();
+        new_opened.push(&neighbor.0);
+        values.push(permutations(
+            n,
+            minute + neighbor.1,
+            &new_opened,
+            valves,
+            finish_on,
+        ));
+    }
+
+    if values.is_empty() {
+        return 0;
+    }
+
+    *values.iter().max().unwrap()
 }
 
 fn order_to_pressure(order: &[&String], valves: &ValveHolder) -> u32 {
@@ -112,11 +148,14 @@ fn order_to_pressure(order: &[&String], valves: &ValveHolder) -> u32 {
     for next in order {
         let n_v = valves.get(&next.to_string()).unwrap();
 
-        let travel_time = *c_v.neighbors.iter().filter(|(s, _w)| {
-            s == *next
-        }).map(|(_s, w)| {
-            *w
-        }).collect::<Vec<u32>>().first().unwrap();
+        let travel_time = *c_v
+            .neighbors
+            .iter()
+            .filter(|(s, _w)| s == *next)
+            .map(|(_s, w)| *w)
+            .collect::<Vec<u32>>()
+            .first()
+            .unwrap();
 
         if minutes + travel_time > 30 {
             // stop here
@@ -154,25 +193,27 @@ fn get_possible_next_steps(
 ) -> Vec<String> {
     let cv = valves.get(current).unwrap();
 
-    cv.neighbors.iter().filter(|(s, _v)| {
-        match previous {
-            None => {}
-            Some(prev) => {
-                if prev == s {
-                    return false;
+    cv.neighbors
+        .iter()
+        .filter(|(s, _v)| {
+            match previous {
+                None => {}
+                Some(prev) => {
+                    if prev == s {
+                        return false;
+                    }
                 }
             }
-        }
 
-        true
-    }).map(|(s, _v)| {
-        s.clone()
-    }).collect()
+            true
+        })
+        .map(|(s, _v)| s.clone())
+        .collect()
 }
 
-fn reconstruct_path(came_from: &HashMap<String, String>, current: &String) -> Vec<String> {
-    let mut total_path = vec![current.clone()];
-    let mut c = current.clone();
+fn reconstruct_path(came_from: &HashMap<String, String>, current: &str) -> Vec<String> {
+    let mut total_path = vec![current.to_owned()];
+    let mut c = current.to_owned();
     loop {
         if came_from.contains_key(&c) {
             c = came_from.get(&c).unwrap().clone();
@@ -188,15 +229,11 @@ fn reconstruct_path(came_from: &HashMap<String, String>, current: &String) -> Ve
     total_path.into_iter().rev().collect()
 }
 
-fn astar(
-    start: &String,
-    goal: &String,
-    valves: &ValveHolder,
-) -> Vec<String> {
+fn astar(start: &str, goal: &String, valves: &ValveHolder) -> Vec<String> {
     // The set of discovered nodes that may need to be (re-)expanded.
     // Initially, only the start node is known.
     // This is usually implemented as a min-heap or priority queue rather than a hash-set.
-    let mut open_set = vec![start.clone()];
+    let mut open_set = vec![start.to_owned()];
 
     // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
     // to n currently known.
@@ -204,13 +241,13 @@ fn astar(
 
     // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
     let mut g_score: HashMap<String, u32> = HashMap::new();
-    g_score.insert(start.clone(), 0);
+    g_score.insert(start.to_owned(), 0);
 
     // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
     // how short a path from start to finish can be if it goes through n.
     // fScore := map with default value of Infinity
     let mut f_score: HashMap<String, u32> = HashMap::new();
-    f_score.insert(start.clone(), heur());
+    f_score.insert(start.to_owned(), heur());
 
     // while open_set is not empty
     loop {
@@ -240,7 +277,7 @@ fn astar(
         // println!("{:?} {}", open_set, current);
         open_set.remove(open_set.iter().position(|v| *v == current).unwrap());
 
-        let next_steps = get_possible_next_steps(&current, came_from.get(&current), &valves);
+        let next_steps = get_possible_next_steps(&current, came_from.get(&current), valves);
         for neighbor in &next_steps {
             // for each neighbor of current
             // d(current,neighbor) is the weight of the edge from current to neighbor
@@ -252,9 +289,9 @@ fn astar(
 
             if tentative_g_score
                 < match g_score.get(neighbor) {
-                None => u32::MAX,
-                Some(v) => *v,
-            }
+                    None => u32::MAX,
+                    Some(v) => *v,
+                }
             {
                 // This path to neighbor is better than any previous one. Record it!
                 // cameFrom[neighbor] := current
@@ -262,12 +299,9 @@ fn astar(
 
                 g_score.insert(neighbor.clone(), tentative_g_score);
 
-                f_score.insert(
-                    neighbor.clone(),
-                    tentative_g_score + heur(),
-                );
+                f_score.insert(neighbor.clone(), tentative_g_score + heur());
 
-                if !open_set.contains(&&neighbor) {
+                if !open_set.contains(neighbor) {
                     open_set.push(neighbor.clone());
                 }
             }
@@ -281,6 +315,4 @@ fn astar(
 #[cfg(test)]
 mod tests {
     use crate::{Valve, ValveHolder};
-
-
 }
